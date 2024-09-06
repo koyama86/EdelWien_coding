f281436f0a93085a7a7e01fc4367d123c67a0596
kamata keigo <k.kamata.sys23@morijyobi.ac.jp>
2024-09-06 10:23:34 +0900

    管理者用サイト作成

---
+++ html/adminConf.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="sidebar">
+        <img class="icon" src="../img//top/sideicon.png" alt="">
+        <p class="company">エーデルワインサポート</p>
+        <h3>管理者用ページ</h3>
+       <a href="#">一覧(TOPページ) ></a>
+       <a href="#">新規お知らせ追加 ></a>
+    </div>
+    <form action="">
+        <div class="article">
+            <div class="detail">
+                <h2>ガラス工房森のくに</h2>
+                <a href="delete.php?id=?">削除</a>
+                <div>
+                    
+                </div>
+            </div>
+
+    </form>
+
+    </div>
+</body>
+</html>
