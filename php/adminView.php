26eac80a47f62cbaecc95f1ab65721431bcf48b5
koyama hinata <h.koyama.sys23@morijyobi.ac.jp>
2024-11-07 15:13:31 +0900

    sidebar

--- php/adminView.php
+++ php/adminView.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <Link rel="stylesheet" href="../css/admin.css">
+    <Link rel="stylesheet" href="../css/adminView.css">
     <title>Document</title>
 </head>
 <body>
